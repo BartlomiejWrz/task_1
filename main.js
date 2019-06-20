a35f94d76762d8e30846fab22a4d7eee1d2ec72c
Bartlomiej Wrzesinski <bartlomiej18@op.pl>
2019-06-20 22:06:03 +0200

    fully working sqare

--- main.js
+++ main.js
@@ -1,29 +1,33 @@
 let square = document.createElement('div')
 document.body.appendChild(square)
-var squareMarginLeft = 10;
+var squareMarginLeft = 0;
 let move = true;
+let screenWidth = window.innerWidth;
 
 square.style.marginLeft = squareMarginLeft + "px";
-square.style.height = "50px";
+let squareSize = square.style.height = "50px";
 square.style.width = "50px";
-// square.style.marginRight = "0px";
 square.style.backgroundColor = "green";
-
-
 
 const movingSquare = () => {
 	setInterval(() => {
-		console.log('dupa');
-		square.style.marginLeft
-		if (move) {
-			squareMarginLeft += 10;
-		}
-		square.style.marginLeft = squareMarginLeft + "px";
-	}, 1000)
+		if (squareMarginLeft > screenWidth) {
+			move = !move
 
+		} else if (squareMarginLeft <= 0) {
+			move = !move;
+		}
+
+		if (!move) {
+			squareMarginLeft++;
+		} else {
+			squareMarginLeft--;
+		}
+
+		square.style.marginLeft = squareMarginLeft + "px";
+	}, 1)
 }
 
 movingSquare();
-
 
 
