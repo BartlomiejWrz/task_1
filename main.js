d65e8e3ea4371003617d39277316d3ed35a5d5af
Bartlomiej Wrzesinski <bartlomiej18@op.pl>
2019-06-20 23:08:35 +0200

    changing size

--- main.js
+++ main.js
@@ -1,33 +1,42 @@
 let square = document.createElement('div')
 document.body.appendChild(square)
-var squareMarginLeft = 0;
+
+let squareMarginLeft = 0;
 let move = true;
 let screenWidth = window.innerWidth;
+let squareHeight = 0;
+let squareWidth = 0;
 
 square.style.marginLeft = squareMarginLeft + "px";
-let squareSize = square.style.height = "50px";
-square.style.width = "50px";
 square.style.backgroundColor = "green";
 
 const movingSquare = () => {
 	setInterval(() => {
 		if (squareMarginLeft > screenWidth) {
 			move = !move
-
 		} else if (squareMarginLeft <= 0) {
 			move = !move;
 		}
 
 		if (!move) {
 			squareMarginLeft++;
+			squareHeight = (squareMarginLeft / screenWidth) * 100;
+			squareWidth = (squareMarginLeft / screenWidth) * 100;
 		} else {
 			squareMarginLeft--;
+			squareHeight = (squareMarginLeft / screenWidth) * 100;
+			squareWidth = (squareMarginLeft / screenWidth) * 100;
 		}
-
+		square.style.height = squareHeight + "px";
+		square.style.width = squareWidth + "px";
 		square.style.marginLeft = squareMarginLeft + "px";
 	}, 1)
 }
 
-movingSquare();
+const changingSize = () => {
+
+}
+
+// movingSquare();
 
 
